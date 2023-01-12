82bd8f4cd65ee57ddc8da736d3a4b6a3d5af9718
Tetiana Doroshko <yanisolua@gmail.com>
2023-01-12 14:53:32 +0200

    rename index

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const app = require("./app");
+const mongoose = require("mongoose");
 
-const contactsRouter = require("./routes/api/contacts");
-const authRouter = require("./routes/api/auth");
-const userRouter = require("./routes/api/users");
+const { DB_HOST, PORT = 3000 } = process.env;
 
-const app = express();
-
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
-
-app.use("/users", userRouter);
-app.use("/auth", authRouter);
-app.use("/api/contacts", contactsRouter);
-
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
-});
-
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
-
-module.exports = app;
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Database connection successful on port ${PORT}`);
+    })
+  )
+  .catch((error) => {
+    console.log(`Database connection was failed. ${error.message}`);
+    process.exit(1);
+  });
