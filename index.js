f638989ba6f370bc0d499b82b65f42f6ebd57400
Tetiana Doroshko <yanisolua@gmail.com>
2023-01-12 15:32:31 +0200

    corrects scripts

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const userRouter = require("./routes/api/users");
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
