615afe944ca0a873d8e346dfdc4c5ea217f65684
Tetiana Doroshko <yanisolua@gmail.com>
2023-01-12 15:16:08 +0200

    corrects index

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const app = require("./app");
+const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const contactsRouter = require("./routes/api/contacts");
+const authRouter = require("./routes/api/auth");
+const userRouter = require("./routes/api/users");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/users", userRouter);
+app.use("/auth", authRouter);
+app.use("/api/contacts", contactsRouter);
+app.get("/", (req, res) => {
+  res.json({ message: "server works" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
