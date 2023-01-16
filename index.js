07f02b6648afac34427b2ca18c610a35ad1568a3
Tetiana Doroshko <yanisolua@gmail.com>
2023-01-16 14:46:39 +0200

    corrects routs

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API works" });
+});
+
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
